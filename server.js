3b637b307103c7fb39d9aa75a176bba8f6ccbab5
Amruta957 <deshmukhamruta957@gmail.com>
2026-04-14 15:46:27 +0530

    Implement basic HTTP server in server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  const filePath = path.join(__dirname, 'index.html');
+  fs.readFile(filePath, (err, data) => {
+    if (err) { res.writeHead(404); res.end('Not found'); return; }
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(data);
+  });
+});
+
+server.listen(3000, () => console.log('Server running on port 3000'));
